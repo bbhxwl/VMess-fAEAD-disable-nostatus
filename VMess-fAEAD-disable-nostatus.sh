fbae0421c59225560c64cc621e8494b727a1fb1d
bbhxwl <470138890@qq.com>
2022-05-26 15:24:43 +0800

    Update VMess-fAEAD-disable-nostatus.sh

--- VMess-fAEAD-disable-nostatus.sh
+++ VMess-fAEAD-disable-nostatus.sh
@@ -2,7 +2,7 @@
 
 serFile="/etc/systemd/system/v2ray.service"
 sed -i '/ExecStart/i\Environment="V2RAY_VMESS_AEAD_FORCED=false"' $serFile
-sed -i '/ExecStart/i\Environment="LimitNOFILE=102400"' $serFile
+sed -i '/ExecStart/i\LimitNOFILE=102400' $serFile
 systemctl daemon-reload
 systemctl restart v2ray
 echo OK.
